cb03948fe70557dc6f9b375f700c3f5df36b6aa3
Luca <lucafagiolini@live.it>
2024-02-05 23:53:27 +0100

    snack-1 logic is working

---
+++ snack-1/js/script.js
@@ -0,0 +1,23 @@
+// creiamo un array vuoto 
+
+var numberList = [];
+
+// chiediamo all'utente di inserire dei numeri fino a quando la somma degli stessi non supera 50
+let somma = 0;
+
+do {
+    // chidiamo di inserire un numero all'interno dell'array
+    let number = parseInt(prompt('Inserisci un numero'));
+    numberList.push(number);
+    console.log(numberList);
+
+    // calcoliamo la somma dei numeri all'interno dell'array
+    for (let i = 0; i < numberList.length; i++) {
+        somma += numberList[i];
+        console.log(somma);
+    }
+
+    // chiediamo di inserire un altro numero all'interno dell'array finche la somma non supera 50
+} while (somma < 50)
+
+
