fc8152f65a3e47dc655c7eda674cec25f46db20b
Luca <lucafagiolini@live.it>
2024-02-07 00:19:01 +0100

    snack-4 completato

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -8,17 +8,22 @@ document.getElementById('start-game-btn').addEventListener('click', function () 
         userNumber = parseInt(prompt('Inserisci un numero'));
         // console.log(userNumber + ' è il numero inserito dall\'utente');
 
+
         if (userNumber % 2 === 0) {
             console.log(userNumber)
             console.log('il numero inserito è pari');
+            document.querySelector('#numero-pari').innerHTML = `<span> il numero pari inserito è ${userNumber} </span>`;
 
         }
 
         else {
-            console.log(userNumber + 1 + ' è il numero dispari successivo');
+            console.log(userNumber + 1 + ' è il numero pari successivo')
+            document.querySelector('#numero-successivo').innerHTML = `<span> il successivo numero pari è ${userNumber + 1} </span>`;
 
         }
 
     }
+
+    console.log("exit from while loop");
 
 });    
