8e2f52ac712291ec0271e178304ef6bd5f6b6189
Luca <lucafagiolini@live.it>
2024-02-06 22:25:26 +0100

    array lists created

---
+++ snack-3/js/script.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById('start-game-btn').addEventListener('click', function () {
+
+    const numberList1 = [1, 2, 3, 4, 5];
+    console.log(numberList1);
+
+
+    const numberList2 = [3, 5, 6, 7, 3, 8, 1, 4, 5, 6, 7, 4, 5];
+    console.log(numberList2);
+
+});
+
+
