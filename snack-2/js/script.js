62ee339fe54e55d51f9c4bacd0ceed62c2e961de
Luca <lucafagiolini@live.it>
2024-02-06 21:59:50 +0100

    logic fixed

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,12 +1,36 @@
 // generiamo un numero casuale da 1 a 100
 
-document.querySelector('#number-generator-btn').addEventListener('click', function () {
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(secretNumber);
+document.querySelector('#check-btn').addEventListener('click', function () {
+    let randomNumber;
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber);
+
+    let userNumber;
 
 
+    do {
+
+        userNumber = parseInt(prompt('Inserisci un numero da 1 a 100'));
+        console.log(userNumber);
+        console.log(randomNumber);
+
+        if (userNumber > randomNumber) {
+            console.log('il numero segreto è più basso');
+        }
+
+        else if (userNumber < randomNumber) {
+            console.log('il numero segreto è più alto');
+        }
+
+    } while (userNumber !== randomNumber);
+    console.log('Hai indovinato!');
+    console.log(userNumber);
+    console.log(randomNumber);
 
 });
+
+
 
 
 
